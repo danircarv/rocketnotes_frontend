865ec6c6a3e00351f6e67c69d3f84c4312b8720e
daniel <danielrodriguescarvalho777@gmail.com>
2024-01-16 11:12:29 -0300

    styling profile picture from user

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,15 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Profile>
+        <img src="https://github.com/danircarv.png" alt="Foto do Usuário" />
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Daniel Rodrigues</strong>
+        </div>
+      </Profile>
+    </Container>
+  );
 }
