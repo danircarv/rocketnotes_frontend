534ed0b4bb7a2a08f8a029cacfb755af69f50b5f
daniel <danielrodriguescarvalho777@gmail.com>
2024-01-16 10:59:59 -0300

    creating header component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,12 @@
 import { Container } from "./styles";
-
+import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Rodrigo Gonçalves</span>
+      <Header />
 
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
       <Button title="Voltar" />
     </Container>
   );
