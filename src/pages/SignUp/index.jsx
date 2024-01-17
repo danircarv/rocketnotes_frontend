8a89a8dbe4121b6ecfd1bad5ece00e02384fa5a6
daniel <danielrodriguescarvalho777@gmail.com>
2024-01-17 16:40:59 -0300

    CREATING PAGE SIGNUP

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -3,25 +3,25 @@ import { Input } from "../../components/Input";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Button } from "../../components/Button";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <Background />
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis </p>
 
-        <h2>Faça seu Login</h2>
+        <h2>Crie sua conta</h2>
+        <Input placeholder="Nome" type="text" icon={FiUser} />
 
         <Input placeholder="E-mail" type="text" icon={FiMail} />
 
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button title="Cadastrar" />
+        <Button title="Entrar" />
 
-        <a href="#">Criar conta</a>
+        <a href="#">Voltar para o login</a>
       </Form>
-
-      <Background />
     </Container>
   );
 }
