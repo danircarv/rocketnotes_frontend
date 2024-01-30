64d7c7631b406d38558ab90883c6f3eabe148ab0
daniel <danielrodriguescarvalho777@gmail.com>
2024-01-30 15:12:19 -0300

    updating avatar picture

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,23 @@
 import { RiShutDownLine } from "react-icons/ri";
 import { useAuth } from "../../hooks/auth";
 
+import { api } from "../../services/api";
+
 import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
   return (
     <Container>
       <Profile to="/profile">
-        <img src="https://github.com/danircarv.png" alt="Foto do Usuário" />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Bem-vindo,</span>
-          <strong>Daniel Rodrigues</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 
 import { useAuth } from "../../hooks/auth";
 
+import { api } from "../../services/api";
+
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
 import { Input } from "../../components/Input";
 
 import { Button } from "../../components/Button";
@@ -18,6 +22,12 @@ export function Profile() {
   const [passwordOld, setPasswordOld] = useState();
   const [passwordNew, setPasswordNew] = useState();
 
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+  const [avatar, setAvatar] = useState(avatarUrl);
+  const [avatarFile, setAvatarFile] = useState(null);
+
   async function handleUpdate() {
     const user = {
       name,
@@ -25,7 +35,15 @@ export function Profile() {
       password: passwordNew,
       old_password: passwordOld,
     };
-    await updateProfile({ user });
+    await updateProfile({ user, avatarFile });
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0];
+    setAvatarFile(file);
+
+    const imagePreview = URL.createObjectURL(file);
+    setAvatar(imagePreview);
   }
 
   return (
@@ -37,11 +55,11 @@ export function Profile() {
       </header>
       <Form>
         <Avatar>
-          <img src="https://github.com/danircarv.png" alt="Foto do usuário" />
+          <img src={avatar} alt="Foto do usuário" />
           <label htmlFor="avatar">
             <FiCamera />
 
-            <input id="avatar" type="file" />
+            <input id="avatar" type="file" onChange={handleChangeAvatar} />
           </label>
         </Avatar>
         <Input
