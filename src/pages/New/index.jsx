2c2d422b29583f7399757cbfa911ac2311a11c70
daniel <danielrodriguescarvalho777@gmail.com>
2024-01-30 18:02:55 -0300

    adding links to notes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
@@ -9,6 +10,13 @@ import { Button } from "../../components/Button";
 import { Container, Form } from "./styles";
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  function handleAddLink() {
+    setLinks((prevState) => [...prevState, newLink]);
+    setNewLink("");
+  }
   return (
     <Container>
       <Header />
@@ -23,8 +31,16 @@ export function New() {
           <TextArea placeholder="Observações" />
 
           <Section title="Links úteis">
-            <NoteItem value="rocketseat.com.br" />
-            <NoteItem isNew />
+            {links.map((link, index) => (
+              <NoteItem key={String(index)} value={link} onClick={() => {}} />
+            ))}
+            <NoteItem
+              isNew
+              placeholder="Novo Link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
