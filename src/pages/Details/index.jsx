38e6961b306f873ab075a02f268ff032a02524d3
daniel <danielrodriguescarvalho777@gmail.com>
2024-02-05 18:41:00 -0300

    loading note details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,51 +1,81 @@
+import { useState, useEffect } from "react";
 import { Container, Links, Content } from "./styles";
+import { useParams, useNavigate } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+
+import { api } from "../../services/api";
 
 import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 import { Tag } from "../../components/Tag";
 
 export function Details() {
+  const [data, setData] = useState(null);
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1);
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?");
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`);
+      navigate(-1);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`);
+      setData(response.data);
+    }
+
+    fetchNote();
+  }, []);
+
   return (
     <Container>
       <Header />
+      {data && (
+        <main>
+          <Content>
+            <ButtonText title="Excluir nota" onClick={handleRemove} />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir nota" />
+            <h1>{data.title}</h1>
 
-          <h1>Introdução ao React</h1>
+            <p>{data.description}</p>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus, accusamus. Aspernatur sed voluptas distinctio
-            officia voluptatum alias blanditiis laudantium optio eum. Libero
-            error minus aliquam animi voluptatum debitis dolorum inventore.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-            ullam! Unde consequuntur officia alias dolorem quo maiores facere,
-            libero quia quos saepe at laborum reprehenderit iusto suscipit
-            fugiat culpa modi!
-          </p>
+            {data.links && (
+              <Section title="Links úteis">
+                <Links>
+                  {data.links.map((link) => (
+                    <li key={String(link.id)}>
+                      <a href={link.url} target="_blank">
+                        {link.url}
+                      </a>
+                    </li>
+                  ))}
+                </Links>
+              </Section>
+            )}
 
-          <Section title="Links úteis">
-            <Links>
-              <li>
-                <a href="#">rocketseat.com.br</a>
-              </li>
-              <li>
-                <a href="#">rocketseat.com.br</a>
-              </li>
-            </Links>
-          </Section>
+            {data.tags && (
+              <Section title="Marcadores">
+                {data.tags.map((tag) => (
+                  <Tag key={String(tag.id)} title={tag.name} />
+                ))}
+              </Section>
+            )}
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="nodejs" />
-          </Section>
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            <Button title="Voltar" onClick={handleBack} />
+          </Content>
+        </main>
+      )}
     </Container>
   );
 }
